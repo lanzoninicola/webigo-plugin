bed934233a406b49275901d810c1433905bc16cf
Nicola <lanzoni.nicola@gmail.com>
2021-09-16 17:02:39 -0300

    Created Settings Class for Archive Product module

---
+++ modules/archive-product/includes/class-webigo-woo-archive-product-settings.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Webigo_Archive_Product_Settings {
+
+    const PODS_PRODUCT_WA_YESNO_FIELD = 'product_wa_contact';
+
+    const PODS_PRODUCT_WA_NUMBER_FIELD = 'product_wa_number';
+
+    const PODS_PRODUCT_WA_TEXT_FIELD = 'product_wa_text';
+
+    const PODS_PRODUCT_WA_TEXT_FALLBACK = 'Para este produto, você precisa entrar em contato com nossa equipe. Pressione o botão WhatsApp e entre em contato conosco.';
+
+    const PRODUCT_WA_PREFIX_CUSTOMER_MSG = 'Olá, estou entrando em contato com você porque estou interessado na compra do produto';
+
+    // const AJAX_CEP_VERIFICATION_ACTION_NAME = "shipping_area_validation";
+
+    // const AJAX_CEP_VERIFICATION_DATA = array(
+    //     'action'     => FILTER_SANITIZE_STRING,
+    //     'nonce'      => FILTER_SANITIZE_STRING,
+    //     'resource'   => FILTER_SANITIZE_STRING,
+    //     'country'    => FILTER_SANITIZE_STRING,
+    //     'state'      => FILTER_SANITIZE_STRING,
+    //     'postcode'   => FILTER_SANITIZE_STRING,
+    // );
+
+}
